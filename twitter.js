f2c69658bbd95481a001fb636e5fb6c2e7645d20
Gregory Machon <gregorymachon@users.noreply.github.com>
2022-02-15 17:47:31 +0000

    Create twitter.js

---
+++ twitter.js
@@ -0,0 +1,11 @@
+// Twitter
+let PromotedContentCleanUp = () => {
+  document.querySelectorAll("main article span").forEach((item) => {
+    if (item.innerHTML.trim() === "Promoted") {
+      item.closest('article').remove();
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", PromotedContentCleanUp);
+document.querySelector("main").addEventListener("DOMNodeInserted", PromotedContentCleanUp);
