8e0de3bffcd48a9052d7d7b2a46fdbfede9d39e8
Gregory Machon <gregorymachon@users.noreply.github.com>
2022-02-15 16:45:25 +0000

    Update linkedin.js

--- linkedin.js
+++ linkedin.js
@@ -1,9 +1,7 @@
 let LinkedInCleanUp = () => {
   document.querySelectorAll("span.feed-shared-actor__description, span.feed-shared-actor__sub-description").forEach((item) => {
     if (item.innerHTML.trim() === "Promoted") {
-      let r = item.parentElement.parentElement.parentElement.parentElement;
-      console.log(r);
-      r.remove();
+      item.parentElement.parentElement.parentElement.parentElement.remove();
     }
   });
 };
