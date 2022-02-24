c0c80691efcc990277dd21252e2e907c655a80af
Gregory Machon <gregorymachon@users.noreply.github.com>
2022-02-24 17:51:24 +0000

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -1,11 +1,13 @@
 // Twitter
 let PromotedContentCleanUp = () => {
   document.querySelectorAll("main article span").forEach((item) => {
-    if (item.innerHTML.trim() === "Promoted") {
-      item.closest('article').remove();
+    if (item.innerHTML.trim().indexOf("Promoted") === 0) {
+      item.closest('.css-1dbjc4n.r-j5o65s.r-qklmqi.r-1adg3ll.r-1ny4l3l').remove(); // Promoted tweet
+      item.closest('.css-1dbjc4n.r-1adg3ll.r-1ny4l3l').remove(); // Promoted Who to follow
     }
   });
 };
 
+PromotedContentCleanUp();
 document.addEventListener("DOMContentLoaded", PromotedContentCleanUp);
-document.addEventListener("DOMNodeInserted", PromotedContentCleanUp);
+new MutationObserver(PromotedContentCleanUp).observe(document.body, { attributes: false, childList: true, subtree: true });
